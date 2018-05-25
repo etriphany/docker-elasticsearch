4438ba9d64f5d327ab999082b17cdd4797456517
cadu-goncalves <cadu.goncalves@gmail.com>
2018-05-25 16:32:36 -0300

    Avoid data volumes from being owned by root

--- es/bin/docker-elasticsearch.sh
+++ es/bin/docker-elasticsearch.sh
@@ -3,17 +3,17 @@
 # Elasticsearch settings array (-a)
 declare -a es_settings
 
-# Iterate over env variables 
+# Iterate over env variables
 # (IFS='=' means words separator is '=')
 while IFS='=' read -r env_key env_val; do
 
-    # Check if key matches (=~) 'word_1.word_2' as expected by 
+    # Check if key matches (=~) 'word_1.word_2' as expected by
     # valid Elasticsearch settings
     if [[ "$env_key" =~ ^[a-z]+\.[a-z]+ ]]; then
 
         # Check if value is not empty (! -z)
         if [[ ! -z $env_val ]]; then
-          # Both key and value seems valid, nice!	
+          # Both key and value seems valid, nice!
           # Store it prefixing with '-E' (docs about it are almost hidden)
           es_settings+=("-E${env_key}=${env_val}")
         fi
